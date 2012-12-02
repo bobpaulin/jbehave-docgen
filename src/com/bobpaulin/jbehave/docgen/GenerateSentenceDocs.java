d0391b7bfe41d38feacc2c35e26c7819f31116bf
bpaulin <bob@bobpaulin.com>
2012-12-01 22:25:56 -0600

    Refactor commandline

--- src/com/bobpaulin/jbehave/docgen/GenerateSentenceDocs.java
+++ src/com/bobpaulin/jbehave/docgen/GenerateSentenceDocs.java
@@ -23,88 +23,94 @@ import japa.parser.ast.expr.NormalAnnotationExpr;
 import japa.parser.ast.expr.SingleMemberAnnotationExpr;
 import japa.parser.ast.visitor.VoidVisitorAdapter;
 
-
 public class GenerateSentenceDocs {
 	public static void main(String[] args) throws Exception {
-        File f = new File("C:\\Users\\bpaulin\\git\\retireup-autotesting\\retireup-jbehave\\src\\main\\java\\com\\retireup\\jbehave\\steps\\RetireUpSteps.java");
-        inspectJavaFile(f);
-    }
 
-    public static void inspectJavaFile(File pFile) 
-    throws FileNotFoundException, ParseException, IOException {
-        CompilationUnit cu;
-        FileInputStream in = new FileInputStream(pFile);
-        try {
-            cu = JavaParser.parse(in);
-        } finally {
-            in.close();
-        }
-        Map<String, List<SentenceData>> jbehavesentenceMap = new HashMap<String, List<SentenceData>>();
-        
-        new MethodVisitor().visit(cu, jbehavesentenceMap);
-        
-        System.out.println(jbehavesentenceMap);
-        
-        File output = new File("output.txt");
-        FileWriter fileWriter = new FileWriter(output);
-        for(String curkey:jbehavesentenceMap.keySet())
-        {
-        	fileWriter.write(curkey);
-        	for(SentenceData cursentence:jbehavesentenceMap.get(curkey))
-        	{
-        		fileWriter.write("\n\n");
-        		if(cursentence.getCommentText() != null)
-        		{
-        			fileWriter.write(cursentence.getCommentText() +"\n");
-        		}
-        		fileWriter.write(curkey + " " + cursentence.getsentenceText() + "\n");
-        		
-        	}
-        	fileWriter.write("\n\n");
-        }
-        
-        fileWriter.flush();
-        fileWriter.close();
-        
-    }
+		String inputFile = args[0];
+		String outputFile = args[1];
 
-    /**
-     * Simple visitor implementation for visiting MethodDeclaration nodes.
-     */
-    private static class MethodVisitor extends VoidVisitorAdapter {
+		File f = new File(inputFile);
+		Map<String, List<SentenceData>> jbehaveSentenceMap = new HashMap<String, List<SentenceData>>();
+		CompilationUnit jbhaveSentenceJavaCompilationUnit = readCompilationUnitFromFile(
+				f, jbehaveSentenceMap);
+		new MethodVisitor().visit(jbhaveSentenceJavaCompilationUnit,
+				jbehaveSentenceMap);
+		writeOutputToFile(outputFile, jbehaveSentenceMap);
+	}
 
-        @Override
-        public void visit(MethodDeclaration n, Object arg) {
-            Map<String, List<SentenceData>> jbehavesentenceMap = (Map<String, List<SentenceData>>) arg;
-            
-            String comment = null;
-            if(n.getJavaDoc() != null)	
-            {
-            	comment = n.getJavaDoc().getContent();
-            }
-            
-            if (n.getAnnotations() != null) {
-            	String sentenceType = null;
-                for (AnnotationExpr annotation : n.getAnnotations()) {
-                    
-                	if(sentenceType== null)
-                		sentenceType = annotation.getName().toString();
-                	
-                	List<SentenceData> sentenceList = jbehavesentenceMap.get(sentenceType);
-                	if(sentenceList == null)
-                	{
-                		sentenceList = new ArrayList<SentenceData>();
-                		jbehavesentenceMap.put(sentenceType, sentenceList);
-                	}
-                	 SingleMemberAnnotationExpr sma = (SingleMemberAnnotationExpr) annotation;
-                	 String restOfsentence = sma.getMemberValue().toString();
-                	 restOfsentence = restOfsentence.substring(1, restOfsentence.length() -1);
-                	 restOfsentence  = restOfsentence.replaceAll("\\\\\"", "\"");
-                	
-                	sentenceList.add(new SentenceData(restOfsentence, comment));
-                   
-                }
-            }
-        }
-    }
+	public static CompilationUnit readCompilationUnitFromFile(File pFile,
+			Map<String, List<SentenceData>> jbehaveSentenceMap)
+			throws FileNotFoundException, ParseException, IOException {
+		CompilationUnit cu;
+		FileInputStream in = new FileInputStream(pFile);
+		try {
+			cu = JavaParser.parse(in);
+		} finally {
+			in.close();
+		}
+		return cu;
+	}
+
+	private static void writeOutputToFile(String outputFilePath,
+			Map<String, List<SentenceData>> jbehavesentenceMap)
+			throws IOException {
+		File output = new File(outputFilePath);
+		FileWriter fileWriter = new FileWriter(output);
+		for (String curkey : jbehavesentenceMap.keySet()) {
+			fileWriter.write(curkey);
+			for (SentenceData cursentence : jbehavesentenceMap.get(curkey)) {
+				fileWriter.write("\n\n");
+				if (cursentence.getCommentText() != null) {
+					fileWriter.write(cursentence.getCommentText() + "\n");
+				}
+				fileWriter.write(curkey + " " + cursentence.getsentenceText()
+						+ "\n");
+
+			}
+			fileWriter.write("\n\n");
+		}
+
+		fileWriter.flush();
+		fileWriter.close();
+	}
+
+	/**
+	 * Simple visitor implementation for visiting MethodDeclaration nodes.
+	 */
+	private static class MethodVisitor extends VoidVisitorAdapter {
+
+		@Override
+		public void visit(MethodDeclaration n, Object arg) {
+			Map<String, List<SentenceData>> jbehavesentenceMap = (Map<String, List<SentenceData>>) arg;
+
+			String comment = null;
+			if (n.getJavaDoc() != null) {
+				comment = n.getJavaDoc().getContent();
+			}
+
+			if (n.getAnnotations() != null) {
+				String sentenceType = null;
+				for (AnnotationExpr annotation : n.getAnnotations()) {
+
+					if (sentenceType == null)
+						sentenceType = annotation.getName().toString();
+
+					List<SentenceData> sentenceList = jbehavesentenceMap
+							.get(sentenceType);
+					if (sentenceList == null) {
+						sentenceList = new ArrayList<SentenceData>();
+						jbehavesentenceMap.put(sentenceType, sentenceList);
+					}
+					SingleMemberAnnotationExpr sma = (SingleMemberAnnotationExpr) annotation;
+					String restOfsentence = sma.getMemberValue().toString();
+					restOfsentence = restOfsentence.substring(1,
+							restOfsentence.length() - 1);
+					restOfsentence = restOfsentence.replaceAll("\\\\\"", "\"");
+
+					sentenceList.add(new SentenceData(restOfsentence, comment));
+
+				}
+			}
+		}
+	}
 }
